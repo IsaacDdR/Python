b8328e204b8249755ee18fccbac469507bb1a4f9
IsaacDdR <isaacovic00@gmail.com>
2018-10-21 12:30:29 -0500

    Ejercicios Python

---
+++ MachineLearn/checkModules.py
@@ -0,0 +1,25 @@
+#Check the versions of libraries
+
+#Python versions
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+
+print ('scipy: {}'.format(scipy.__version__))
+
+import numpy
+
+print('numpy: {}'.format(numpy.__version__))
+
+import matplotlib
+
+print ('matplotlib: {}'.format(matplotlib.__version__))
+
+import pandas
+
+print ('Pandas: {}'.format(pandas.__version__))
+
+import sklearn
+
+print ('sklearn: {}'.format(sklearn.__version__))
